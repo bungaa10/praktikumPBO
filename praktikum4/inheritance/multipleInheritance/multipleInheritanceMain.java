34df396c4e64c88d91dd1e976525ff7f6ba1e9b1
bungaa10 <bungaarini72@gmail.com>
2025-10-01 07:50:43 +0700

    2403047_bungaarini

--- praktikum4/inheritance/multipleInheritance/multipleInheritanceMain.java
+++
@@ -1,9 +0,0 @@
-package multipleInheritance;
-
-public class multipleInheritanceMain {
-    public static void main(String[] args) {
-        BukuPerpustakaan buku = new BukuPerpustakaan("Belajar Java", "Andi");
-        buku.pinjam();      
-        buku.kembalikan();  
-    }
-}
--- praktikum4/inheritance/singleInheritance/sinleinheritanceMain.java
+++
@@ -1,7 +0,0 @@
-public class sinleinheritanceMain {
-    public static void main(String[] args) {
-        singleInheritance bukuPelajaran = new singleInheritance();
-        singleInheritance.BukuPelajaran pelajaran = bukuPelajaran.new BukuPelajaran("Matematika", "Dr. Math", "Matematika");
-        pelajaran.infoPelajaran();
-    }
-}
